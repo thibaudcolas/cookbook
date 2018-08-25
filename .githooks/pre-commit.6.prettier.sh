ad62a795a4f5e8aa66240dbb32b3400e1ac3e071
Thibaud Colas <thibaudcolas@gmail.com>
2018-08-25 22:30:14 +0300

    chore(githooks): add basic git hooks to the cookbook

---
+++ .githooks/pre-commit.6.prettier.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Format and re-stage fully staged files only.
+
+if [ -n "$JS_FULLY_STAGED" ];
+then
+  npx prettier --write $JS_FULLY_STAGED
+  git add $JS_FULLY_STAGED
+fi
+
+if [ -n "$SCSS_FULLY_STAGED" ];
+then
+  npx prettier --write $SCSS_FULLY_STAGED
+  git add $SCSS_FULLY_STAGED
+fi
+
+if [ -n "$MD_FULLY_STAGED" ];
+then
+  npx prettier --write $MD_FULLY_STAGED
+  git add $MD_FULLY_STAGED
+fi
+
+if [ -n "$MD_STAGED" ];
+then
+    npx prettier --list-different $MD_STAGED
+fi
+
+if [ -n "$JSON_FULLY_STAGED" ];
+then
+  npx prettier --write $JSON_FULLY_STAGED
+  git add $JSON_FULLY_STAGED
+fi
+
+if [ -n "$JSON_STAGED" ];
+then
+    npx prettier --list-different $JSON_STAGED
+fi
+
+if [ -n "$YAML_FULLY_STAGED" ];
+then
+  npx prettier --write $YAML_FULLY_STAGED
+  git add $YAML_FULLY_STAGED
+fi
+
+if [ -n "$YAML_STAGED" ];
+then
+    npx prettier --list-different $YAML_STAGED
+fi
---
+++ .githooks/pre-commit.8.test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+# if [ -n "$JS_STAGED" ] || [ -n "$SCSS_STAGED" ];
+# then
+#   npm run test -s
+# fi
